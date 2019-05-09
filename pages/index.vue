325d9cd9c09cea3443084f4f0d532c65beb6a504
Alex <alex@orion.sh>
2019-05-09 11:24:37 +0200

    plugins

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,19 @@
   <section>
     <b-container>
       <b-alert show>Bienvenue</b-alert>
+      <multiselect v-model="value" :options="options"></multiselect>
     </b-container>
   </section>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      value: null,
+      options: ['React', 'Angular', 'Vue']
+    }
+  }
 }
 </script>
 
